440d6234fd65fe805918648b8294755d105423bc
Tran Bao Toan <kztoan01@gmail.com>
2023-12-06 23:27:02 +0700

    fixed cv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@
 // import ArtHub from '@/components/ArtHub'
 // import Speaking from '@/components/Speaking'
 // import ScrollToTop from '@/components/ScrollToTop'
+import toan2 from  '../../public/assets/images/profile-pic.jpg'
 import { AcademicCapIcon } from '@heroicons/react/solid';
 import Card3D from '@/components/Card3D'
 import { motion, useTransform, useScroll } from "framer-motion";
@@ -296,7 +297,7 @@ export default function Home() {
                           </dl>
                         </li>
                       ))}
-                    </ol><a href='https://drive.google.com/file/d/1TYeg0L0RqKMiymfKsEFMs0kKiGUFBvNV/view?usp=sharing' target="_blank"
+                    </ol><a href='https://drive.google.com/file/d/1DkX1TUYnG6yKtHL1IYl-sI4Vr-M1JpmM/view?usp=sharing' target="_blank"
                       className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
                     >Download CV<svg viewBox="0 0 16 16" fill="none" aria-hidden="true"
                       className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50">
